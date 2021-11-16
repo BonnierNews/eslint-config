7f26196d848bbfa676c3c6d8dcdc04b566f81b74
Markus Ekholm <markus.ekholm@bonniernews.se>
2021-11-16 13:35:52 +0100

    add new-cap in standard not only test (#2)
    * add new-cap in standard not only test

--- index.js
+++ index.js
@@ -202,7 +202,8 @@ const rules = {
   "require-await": "error", // make it explicit
   "object-shorthand": [ "error", "properties" ], // make it short
   "object-curly-spacing": [ "error", "always" ], // consistency
-  "array-bracket-spacing": [ "error", "always" ] // consistency with above
+  "array-bracket-spacing": [ "error", "always" ], // consistency with above
+  "new-cap": "error" // consistency with test rules
 };
 
 module.exports = {
