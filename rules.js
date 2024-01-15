76e389042ebb0ed00ddfe4b3e4293463fdaa6226
Felix Laxvik <felix.laxvik@bonniernews.se>
2024-01-15 12:00:45 +0100

    Fix tests and formatting

--- rules.js
+++ rules.js
@@ -276,15 +276,7 @@ module.exports = function getRules(isModuleProject) {
     "require-await": "error", // make it explicit
     "object-shorthand": [ "error", "properties" ], // make it short
     "object-curly-spacing": [ "error", "always" ], // consistency
-    "react/jsx-curly-spacing": [
-      "error",
-      {
-        when: "never",
-        children: {
-          when: "always",
-        },
-      },
-    ],
+    "react/jsx-curly-spacing": [ "error", { when: "never", children: { when: "always" } } ],
     "array-bracket-spacing": [ "error", "always" ], // consistency with above
     "new-cap": "error", // consistency with test rules
     "switch-colon-spacing": "error",
