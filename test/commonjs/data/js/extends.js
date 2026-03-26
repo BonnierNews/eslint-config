0ca2ee011564dd1637f2a4bdfb378188cdb08dc6
MattiasOlla <mattias.olla@bonniernews.se>
2026-03-26 15:24:07 +0100

    Add tests that it works with commonjs as well

---
+++ test/commonjs/data/js/extends.js
@@ -0,0 +1,18 @@
+"use strict";
+class Foo {
+  constructor(bar) {
+    this.bar = bar;
+  }
+}
+
+class Bar extends Foo {
+  foo(foo) {
+    this.bar = foo;
+  }
+}
+
+function fooer() {
+  return new Bar("test");
+}
+
+fooer();
---
+++ test/commonjs/data/js/noerrors.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function add(a, b) {
+  return a + b;
+}
+
+add(2, 3);
